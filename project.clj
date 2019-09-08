511780aea7a1d4628952fbf0ddcf4eee3398b753
neiro <neiro.mail@gmail.com>
2019-09-08 12:15:45 +0300

    Install Expound
    To show human-readable spec errors.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
                  [environ "1.1.0"]
+                 [expound "0.7.2"]
                  [io.pedestal/pedestal.jetty "0.5.7"]
                  [io.pedestal/pedestal.service "0.5.7"]
                  [morse "0.4.3"]
@@ -15,7 +16,10 @@
                  [org.slf4j/jcl-over-slf4j "1.7.26"]
                  [org.slf4j/jul-to-slf4j "1.7.26"]
                  [org.slf4j/log4j-over-slf4j "1.7.26"]]
-  :plugins [[lein-environ "1.1.0"]]
+  :plugins [[lein-environ "1.1.0"]
+            [lein-cljfmt "0.6.3"]
+            [lein-kibit "0.1.6"]
+            [lein-figwheel "0.5.18"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "social-stats-bot.server/run-dev"]}
