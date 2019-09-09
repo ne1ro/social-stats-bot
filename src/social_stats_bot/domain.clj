13db7271f66dbc0d6202531c0803a27d88814361
neiro <neiro.mail@gmail.com>
2019-09-09 16:20:13 +0300

    Prototype domain model

---
+++ src/social_stats_bot/domain.clj
@@ -0,0 +1,30 @@
+(ns social-stats-bot.domain
+  (:require [clojure.spec.alpha :as s]
+            [expound.alpha :as expound]))
+
+;; User attributes
+(s/def ::provider #{:instagram})
+(s/def ::followings-count pos-int?)
+(s/def ::posts-count pos-int?)
+(s/def ::followers-count pos-int?)
+(s/def ::avatar string?)
+(s/def ::first-name string?)
+(s/def ::last-name string?)
+(s/def ::nickname string?)
+(s/def ::last-fetched-at inst?)
+(s/def ::inserted-at inst?)
+
+(s/def ::user
+  (s/keys :req [::first-name ::provider ::nickname]
+          :opt [::last-name ::avatar ::posts-count
+                :followers-count ::followings-count
+                ::last-fetched-at ::inserted-at]))
+
+;; Stats attributes
+(s/def ::start-date inst?)
+(s/def ::end-date inst?)
+(s/def ::split-by #{:month :day :week})
+(s/def ::graph string?)
+
+(s/def ::stats-params (s/keys :req [::start-date ::end-date ::split-by]))
+(s/def ::stats (s/keys :req [::user ::graph ::stats-params]))
