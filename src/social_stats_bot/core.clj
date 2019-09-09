b9d36f43c4da2a69c3af09e282e8a2545f8ce13e
neiro <neiro.mail@gmail.com>
2019-09-09 17:23:40 +0300

    Move server & service to web

---
+++ src/social_stats_bot/core.clj
@@ -0,0 +1,13 @@
+(ns social-stats-bot.core
+  "Application layer of social stats bot"
+  (:require [social-stats-bot.domain :as domain]))
+
+(defn get-user
+  "Gets a user by his or her nickname and social acc provider"
+  [nickname provider]
+  {})
+
+(defn get-stats
+  "Gets stats for a social account by user nickname and provider"
+  [nickname provider]
+  {})
