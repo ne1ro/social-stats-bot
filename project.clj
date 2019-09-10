58d0f60d36e3ca83516343d0da127eb00aa5867a
neiro <neiro.mail@gmail.com>
2019-09-10 15:41:22 +0300

    Use Integrant instead of mount

--- project.clj
+++ project.clj
@@ -5,13 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
+                 [com.github.javafaker/javafaker "0.16"]
                  [dorothy "0.0.7"]
-                 [environ "1.1.0"]
                  [expound "0.7.2"]
+                 [integrant "0.7.0"]
+                 [io.aviso/pretty "0.1.37"]
                  [io.pedestal/pedestal.jetty "0.5.7"]
                  [io.pedestal/pedestal.service "0.5.7"]
                  [morse "0.4.3"]
-                 [mount "0.1.16"]
                  [orchestra "2018.12.06-2"]
                  [org.clojure/core.async "0.4.490"]
                  [org.jsoup/jsoup "1.12.1"]
@@ -21,7 +22,9 @@
   :plugins [[lein-environ "1.1.0"]
             [lein-cljfmt "0.6.4"]
             [lein-kibit "0.1.6"]
+            [io.aviso/pretty "0.1.37"]
             [lein-figwheel "0.5.18"]]
+  :middleware [io.aviso.lein-pretty/inject]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "web.server/run-dev"]}
