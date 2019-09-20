9f24d446109e95825a31c904283c48ff45af8159
neiro <neiro.mail@gmail.com>
2019-09-20 16:26:45 +0300

    Create a test configuration

--- src/messenger/test.clj
+++ src/messenger/test.clj
@@ -1,2 +1,15 @@
-(ns messenger.test-adapter
-  "Test adapter for the messenger port")
+(ns messenger.messenger-local
+  "Test adapter for the messenger port"
+  (:require [social-stats-bot.messenger :refer [Messenger]]
+            [integrant.core :as ig]))
+
+(defrecord MessengerLocal
+    [conf]
+  Messenger
+
+  (set-endpoint [_conf _] nil)
+  (send-message [_conf m _chat-id] m)
+  (send-photo [_conf photo _chat-id] photo))
+
+(defmethod ig/init-key :messenger-local [_ conf] (->MessengerLocal conf))
+(defmethod ig/halt-key! :messenger-local [_ conf] nil)
