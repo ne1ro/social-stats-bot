ce9be19ae7f9936a718195125ccf11102ba310da
neiro <neiro.mail@gmail.com>
2019-09-30 16:39:02 +0300

    Remove user.clj

--- project.clj
+++ project.clj
@@ -3,20 +3,22 @@
   :url "http://t.me/SocialStatsBot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clj-http "3.10.0"]
-                 [com.datomic/datomic-free "0.9.5656"]
-                 [com.github.javafaker/javafaker "0.16"]
-                 [compojure "1.6.1"]
-                 [dorothy "0.0.7"]
-                 [expound "0.7.2"]
-                 [integrant "0.7.0"]
-                 [io.aviso/pretty "0.1.37"]
-                 [morse "0.4.3"]
-                 [org.clojure/core.async "0.4.490"]
-                 [ring/ring-jetty-adapter "1.7.1"]
-                 [ring/ring-json "0.5.0"]
-                 [ring-logger "1.0.1"]]
+  :dependencies [
+		 [org.clojure/clojure "1.10.1"]
+     [clj-http "3.10.0"]
+     [com.datomic/datomic-free "0.9.5656"]
+     [com.github.javafaker/javafaker "0.16"]
+     [compojure "1.6.1"]
+     [dorothy "0.0.7"]
+     [expound "0.7.2"]
+     [integrant "0.7.0"]
+     [io.aviso/pretty "0.1.37"]
+     [morse "0.4.3"]
+     [org.clojure/core.async "0.4.490"]
+     [ring-logger "1.0.1"]
+     [ring/ring-jetty-adapter "1.7.1"]
+     [ring/ring-json "0.5.0"]
+]
   :plugins [[lein-environ "1.1.0"]
             [lein-cljfmt "0.6.4"]
             [lein-kibit "0.1.6"]
@@ -26,7 +28,7 @@
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:resource-paths ["resources/dev"]
-                   :dependencies [[integrant/repl "0.3.1"] [lein-cljfmt "0.6.4"]]}
+                   :dependencies [[lein-cljfmt "0.6.4"]]}
              :test {:resource-paths ["resources/test"]
                     :dependencies [[lein-cloverage "1.1.1"]]}
              :prod {:resource-paths ["resources/prod"]}
--- src/user.clj
+++
@@ -1,7 +0,0 @@
-(ns user
-  (:require [integrant.repl :refer [clear go halt prep init reset reset-all]]
-            [social-stats-bot.core :as core]))
-
-(integrant.repl/set-prep! (-> "dev" core/config constantly))
-
-(prep)
